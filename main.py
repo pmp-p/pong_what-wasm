00c97951f73aedb9d4dada1af6f17c1406f995c6
pmp-p <mail.peny@free.fr>
2023-08-28 22:01:27 +0200

    pygbag test

--- main.py
+++ main.py
@@ -1,238 +1,244 @@
+import pygbag.aio as asyncio
+
 import pygame
 import random
 import math
 
 from spark import *
 
-SCREEN_SIZE = pygame.Vector2(960, 540)
+async def main():
+    SCREEN_SIZE = pygame.Vector2(960, 540)
 
-pygame.init()
+    pygame.init()
 
-sparks = []
+    sparks = []
 
-class Ball:
+    class Ball:
 
-    def __init__(self, position : pygame.Vector2):
+        def __init__(self, position : pygame.Vector2):
 
-        self.rect = pygame.FRect(position, [15, 15])
+            self.rect = pygame.FRect(position, [15, 15])
 
-        self.surface = pygame.Surface([15, 15], pygame.SRCALPHA)
-        pygame.draw.circle(self.surface, [233, 245, 249], [8, 8], 7)
+            self.surface = pygame.Surface([15, 15], pygame.SRCALPHA)
+            pygame.draw.circle(self.surface, [233, 245, 249], [8, 8], 7)
 
-        random_angle = random.uniform(3*math.pi/4, 5*math.pi/4) - math.pi * (1 - random.randint(0, 1))
-        self.direction = pygame.Vector2(math.cos(random_angle), math.sin(random_angle))
+            random_angle = random.uniform(3*math.pi/4, 5*math.pi/4) - math.pi * (1 - random.randint(0, 1))
+            self.direction = pygame.Vector2(math.cos(random_angle), math.sin(random_angle))
 
-        self.movement = pygame.Vector2(0,0)
+            self.movement = pygame.Vector2(0,0)
 
-        self.speed = 400
+            self.speed = 400
 
-# def generate_shadows(objects : list[pygame.FRect, pygame.Surface]) -> list[pygame.Surface, tuple]:
+    # def generate_shadows(objects : list[pygame.FRect, pygame.Surface]) -> list[pygame.Surface, tuple]:
 
-#     shadows = []
+    #     shadows = []
 
-#     for rect, surf in objects:
-        
-#         pos = rect.topleft + pygame.Vector2(5, 5)
-#         mask = pygame.mask.from_surface(surf, 0)
-#         shadows.append([mask.to_surface(setcolor=(0, 0, 0, 255), unsetcolor=(0, 0, 0, 0)), pos])
+    #     for rect, surf in objects:
 
-#     return shadows
+    #         pos = rect.topleft + pygame.Vector2(5, 5)
+    #         mask = pygame.mask.from_surface(surf, 0)
+    #         shadows.append([mask.to_surface(setcolor=(0, 0, 0, 255), unsetcolor=(0, 0, 0, 0)), pos])
 
-def generate_sparks(position : pygame.Vector2, normal : pygame.Vector2):
+    #     return shadows
 
-    normal_angle = math.atan2(normal.y, normal.x)
+    def generate_sparks(position : pygame.Vector2, normal : pygame.Vector2):
 
-    for _ in range(5):
-        spark = Spark()
+        normal_angle = math.atan2(normal.y, normal.x)
 
-        spark.position = position.copy()
-        spark.speed = random.uniform(200, 250)
-        spark.speed_scale = spark.speed / 100
-        spark.scale = random.uniform(3.5, 4.5)
-        spark.angle = random.uniform(normal_angle - 2*math.pi/6, normal_angle + 2*math.pi/6)
-        spark.color = pygame.Color(255, 255, 255)
+        for _ in range(5):
+            spark = Spark()
 
-        sparks.append(spark)
+            spark.position = position.copy()
+            spark.speed = random.uniform(200, 250)
+            spark.speed_scale = spark.speed / 100
+            spark.scale = random.uniform(3.5, 4.5)
+            spark.angle = random.uniform(normal_angle - 2*math.pi/6, normal_angle + 2*math.pi/6)
+            spark.color = pygame.Color(255, 255, 255)
 
-def collisions(rect : pygame.FRect, colliders : list[pygame.FRect]) -> list[pygame.FRect]:
+            sparks.append(spark)
 
-    collided = []
+    def collisions(rect : pygame.FRect, colliders : list[pygame.FRect]) -> list[pygame.FRect]:
 
-    for collider in colliders:
-        if rect.colliderect(collider):
-            collided.append(collider)
-    
-    return collided
+        collided = []
 
-def collision_resolution(ball : Ball, colliders : list[pygame.FRect]) -> None:
+        for collider in colliders:
+            if rect.colliderect(collider):
+                collided.append(collider)
 
-    ball.rect.x += ball.movement.x
+        return collided
 
-    score_result = -1
+    def collision_resolution(ball : Ball, colliders : list[pygame.FRect]) -> None:
 
-    if(ball.rect.left < 0):
-        ball.rect.left = 0
-        ball.direction.reflect_ip(pygame.Vector2(1, 0))
-        generate_sparks(pygame.Vector2(ball.rect.midleft), pygame.Vector2(1, 0))
-        score_result = 1
-    elif (ball.rect.right > SCREEN_SIZE.x):
-        ball.rect.right = SCREEN_SIZE.x
-        ball.direction.reflect_ip(pygame.Vector2(-1, 0))
-        generate_sparks(pygame.Vector2(ball.rect.midright), pygame.Vector2(-1, 0))
-        score_result = 0
-    
-    for collided in collisions(ball.rect, colliders):
-        if ball.movement.x < 0:
-            ball.rect.left = collided.right
+        ball.rect.x += ball.movement.x
+
+        score_result = -1
+
+        if(ball.rect.left < 0):
+            ball.rect.left = 0
             ball.direction.reflect_ip(pygame.Vector2(1, 0))
-        elif ball.movement.x > 0:
-            ball.rect.right = collided.left
+            generate_sparks(pygame.Vector2(ball.rect.midleft), pygame.Vector2(1, 0))
+            score_result = 1
+        elif (ball.rect.right > SCREEN_SIZE.x):
+            ball.rect.right = SCREEN_SIZE.x
             ball.direction.reflect_ip(pygame.Vector2(-1, 0))
-    
-    ball.rect.y += ball.movement.y
+            generate_sparks(pygame.Vector2(ball.rect.midright), pygame.Vector2(-1, 0))
+            score_result = 0
 
-    if(ball.rect.top < 0):
-        ball.rect.top = 0
-        ball.direction.reflect_ip(pygame.Vector2(0, 1))
-        generate_sparks(pygame.Vector2(ball.rect.midtop), pygame.Vector2(0, 1))
-    elif (ball.rect.bottom > SCREEN_SIZE.y):
-        ball.rect.bottom = SCREEN_SIZE.y
-        ball.direction.reflect_ip(pygame.Vector2(0, -1))
-        generate_sparks(pygame.Vector2(ball.rect.midbottom), pygame.Vector2(0, -1))
-    
-    for collided in collisions(ball.rect, colliders):
-        if ball.movement.y < 0:
-            ball.rect.top = collided.bottom
+        for collided in collisions(ball.rect, colliders):
+            if ball.movement.x < 0:
+                ball.rect.left = collided.right
+                ball.direction.reflect_ip(pygame.Vector2(1, 0))
+            elif ball.movement.x > 0:
+                ball.rect.right = collided.left
+                ball.direction.reflect_ip(pygame.Vector2(-1, 0))
+
+        ball.rect.y += ball.movement.y
+
+        if(ball.rect.top < 0):
+            ball.rect.top = 0
             ball.direction.reflect_ip(pygame.Vector2(0, 1))
-        elif ball.movement.y > 0:
-            ball.rect.bottom = collided.top
+            generate_sparks(pygame.Vector2(ball.rect.midtop), pygame.Vector2(0, 1))
+        elif (ball.rect.bottom > SCREEN_SIZE.y):
+            ball.rect.bottom = SCREEN_SIZE.y
             ball.direction.reflect_ip(pygame.Vector2(0, -1))
-    
-    return score_result
+            generate_sparks(pygame.Vector2(ball.rect.midbottom), pygame.Vector2(0, -1))
 
-screen = pygame.display.set_mode(SCREEN_SIZE)
-pygame.display.set_caption("Pong what")
+        for collided in collisions(ball.rect, colliders):
+            if ball.movement.y < 0:
+                ball.rect.top = collided.bottom
+                ball.direction.reflect_ip(pygame.Vector2(0, 1))
+            elif ball.movement.y > 0:
+                ball.rect.bottom = collided.top
+                ball.direction.reflect_ip(pygame.Vector2(0, -1))
 
-player_1 , player_2 = (pygame.FRect([0, 0], [20, 100]) for _ in range(2))
+        return score_result
 
-player_1.topleft = pygame.Vector2(20, 20)
-player_2.topleft = pygame.Vector2(SCREEN_SIZE.x - player_1.width - 20 , 20)
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption("Pong what")
 
-player_surface = pygame.Surface([20, 100])
-player_surface.fill("white")
+    player_1 , player_2 = (pygame.FRect([0, 0], [20, 100]) for _ in range(2))
 
-ball = Ball(pygame.Vector2())
-ball.rect.center = SCREEN_SIZE / 2
+    player_1.topleft = pygame.Vector2(20, 20)
+    player_2.topleft = pygame.Vector2(SCREEN_SIZE.x - player_1.width - 20 , 20)
 
-controls = {"up_1":False, "up_2":False, "down_1":False, "down_2":False}
+    player_surface = pygame.Surface([20, 100])
+    player_surface.fill("white")
 
-clock = pygame.Clock()
+    ball = Ball(pygame.Vector2())
+    ball.rect.center = SCREEN_SIZE / 2
 
-small_font = pygame.Font("alagard.ttf", 25)
-big_font = pygame.Font("alagard.ttf", 40)
+    controls = {"up_1":False, "up_2":False, "down_1":False, "down_2":False}
 
-score = [0, 0]
+    clock = pygame.Clock()
 
-running = True
+    small_font = pygame.Font("alagard.ttf", 25)
+    big_font = pygame.Font("alagard.ttf", 40)
 
-while running:
+    score = [0, 0]
 
-    dt = clock.tick(60) / 1000
+    running = True
 
-    # event handling
+    while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                controls["up_1"] = True
-            elif event.key == pygame.K_DOWN:
-                controls["down_1"] = True
-            elif event.key == pygame.K_a:
-                controls["up_2"] = True
-            elif event.key == pygame.K_z:
-                controls["down_2"] = True
-        
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                controls["up_1"] = False
-            elif event.key == pygame.K_DOWN:
-                controls["down_1"] = False
-            elif event.key == pygame.K_a:
-                controls["up_2"] = False
-            elif event.key == pygame.K_z:
-                controls["down_2"] = False
+        dt = clock.tick(60) / 1000
 
-    # updating
+        # event handling
 
-    if controls["up_1"]:
-        player_1.y -= 400 * dt
-    elif controls["down_1"]:
-        player_1.y += 400 * dt
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    player_1.y = pygame.math.clamp(player_1.y, 20, SCREEN_SIZE.y - 20 - player_1.height)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    controls["up_1"] = True
+                elif event.key == pygame.K_DOWN:
+                    controls["down_1"] = True
+                elif event.key == pygame.K_a:
+                    controls["up_2"] = True
+                elif event.key == pygame.K_z:
+                    controls["down_2"] = True
 
-    if controls["up_2"]:
-        player_2.y -= 400 * dt
-    elif controls["down_2"]:
-        player_2.y += 400 * dt
-    
-    player_2.y = pygame.math.clamp(player_2.y, 20, SCREEN_SIZE.y - 20 - player_2.height)
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    controls["up_1"] = False
+                elif event.key == pygame.K_DOWN:
+                    controls["down_1"] = False
+                elif event.key == pygame.K_a:
+                    controls["up_2"] = False
+                elif event.key == pygame.K_z:
+                    controls["down_2"] = False
 
-    ball.movement = ball.direction * ball.speed * dt
+        # updating
 
-    result = collision_resolution(ball, [player_1, player_2])
+        if controls["up_1"]:
+            player_1.y -= 400 * dt
+        elif controls["down_1"]:
+            player_1.y += 400 * dt
 
-    if(result != -1):
-        if(result):
-            score[1] += 1
-        else:
-            score[0] += 1
-        
-        random_angle = random.uniform(-math.pi/4, math.pi/4) + math.pi * result
-        ball.direction = pygame.Vector2(math.cos(random_angle), math.sin(random_angle))
-        ball.rect.center = SCREEN_SIZE / 2
+        player_1.y = pygame.math.clamp(player_1.y, 20, SCREEN_SIZE.y - 20 - player_1.height)
 
-    # drawing
+        if controls["up_2"]:
+            player_2.y -= 400 * dt
+        elif controls["down_2"]:
+            player_2.y += 400 * dt
 
-    screen.fill(0x130c2b)
-    
-    display_surface = pygame.Surface(SCREEN_SIZE, pygame.SRCALPHA)
+        player_2.y = pygame.math.clamp(player_2.y, 20, SCREEN_SIZE.y - 20 - player_2.height)
 
-    # screen.blits(shadows)
+        ball.movement = ball.direction * ball.speed * dt
 
-    for spark in sparks:
+        result = collision_resolution(ball, [player_1, player_2])
 
-        spark.update(dt)
-        spark.speed_scale -= 4 * dt
-        spark.speed -= 15.5 * dt
+        if(result != -1):
+            if(result):
+                score[1] += 1
+            else:
+                score[0] += 1
 
-        if spark.speed_scale < 0:
-            sparks.remove(spark)
-        else:
-            spark.draw(display_surface)
+            random_angle = random.uniform(-math.pi/4, math.pi/4) + math.pi * result
+            ball.direction = pygame.Vector2(math.cos(random_angle), math.sin(random_angle))
+            ball.rect.center = SCREEN_SIZE / 2
 
-    display_surface.blit(player_surface, player_1.topleft)
-    display_surface.blit(player_surface, player_2.topleft)
-    
-    pygame.draw.line(screen, [168, 213, 229], (480, 0), (480, 540))
+        # drawing
 
-    display_surface.blit(ball.surface, ball.rect.topleft)
+        screen.fill(0x130c2b)
 
-    fps_text = small_font.render(f"FPS : {round(clock.get_fps(), 2)}", True, "white")
-    display_surface.blit(fps_text, [SCREEN_SIZE.x / 2 - 60, SCREEN_SIZE.y - fps_text.get_height() - 20])
+        display_surface = pygame.Surface(SCREEN_SIZE, pygame.SRCALPHA)
 
-    score_text_player_1 = big_font.render(f"{score[0]}", True, "white")
-    display_surface.blit(score_text_player_1, [SCREEN_SIZE.x / 2 - score_text_player_1.get_width() - 40, 20])
+        # screen.blits(shadows)
 
-    score_text_player_2 = big_font.render(f"{score[1]}", True, "white")
-    display_surface.blit(score_text_player_2, [SCREEN_SIZE.x / 2 + 40, 20])
+        for spark in sparks:
 
-    mask = pygame.mask.from_surface(display_surface, 0)
-    screen.blit(mask.to_surface(setcolor=(0, 0, 0, 255), unsetcolor=(0, 0, 0, 0)), [5, 5])
-    screen.blit(display_surface, [0,0])
-    pygame.display.flip()
+            spark.update(dt)
+            spark.speed_scale -= 4 * dt
+            spark.speed -= 15.5 * dt
 
-pygame.quit()
+            if spark.speed_scale < 0:
+                sparks.remove(spark)
+            else:
+                spark.draw(display_surface)
+
+        display_surface.blit(player_surface, player_1.topleft)
+        display_surface.blit(player_surface, player_2.topleft)
+
+        pygame.draw.line(screen, [168, 213, 229], (480, 0), (480, 540))
+
+        display_surface.blit(ball.surface, ball.rect.topleft)
+
+        fps_text = small_font.render(f"FPS : {round(clock.get_fps(), 2)}", True, "white")
+        display_surface.blit(fps_text, [SCREEN_SIZE.x / 2 - 60, SCREEN_SIZE.y - fps_text.get_height() - 20])
+
+        score_text_player_1 = big_font.render(f"{score[0]}", True, "white")
+        display_surface.blit(score_text_player_1, [SCREEN_SIZE.x / 2 - score_text_player_1.get_width() - 40, 20])
+
+        score_text_player_2 = big_font.render(f"{score[1]}", True, "white")
+        display_surface.blit(score_text_player_2, [SCREEN_SIZE.x / 2 + 40, 20])
+
+        mask = pygame.mask.from_surface(display_surface, 0)
+        screen.blit(mask.to_surface(setcolor=(0, 0, 0, 255), unsetcolor=(0, 0, 0, 0)), [5, 5])
+        screen.blit(display_surface, [0,0])
+        pygame.display.flip()
+        await asyncio.sleep(0)
+    pygame.quit()
+
+if __name__ == '__main__':
+    asyncio.run(main())
